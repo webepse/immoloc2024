23ec570a604bc711499b5635e4e516b0ac4bb2a1
berti <berti@CLASSES.LOCAL>
2023-10-13 15:29:07 +0200

    Cours 4 - collectionType et JS

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\Ad;
+use App\Entity\Image;
 use App\Form\AnnonceType;
 use App\Repository\AdRepository;
 use Doctrine\ORM\EntityManagerInterface;
@@ -42,6 +43,20 @@ class AdController extends AbstractController
     {
 
         $ad = new Ad();
+
+        $image1 = new Image();
+        $image1->setUrl("https://picsum.photos/400/200")
+            ->setCaption('Titre 1');
+    
+        $ad->addImage($image1);
+
+        $image2 = new Image();
+        $image2->setUrl("https://picsum.photos/400/200")
+            ->setCaption('Titre 2');
+
+        $ad->addImage($image2);
+
+
         $form = $this->createForm(AnnonceType::class, $ad);
 
         // $arrayForm = $request->request->all();
--- src/Form/AnnonceType.php
+++ src/Form/AnnonceType.php
@@ -3,14 +3,16 @@
 namespace App\Form;
 
 use App\Entity\Ad;
+use App\Form\ImageType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 use Symfony\Component\Form\Extension\Core\Type\UrlType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
-use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\MoneyType;
+use Symfony\Component\Form\Extension\Core\Type\IntegerType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\CollectionType;
 
 class AnnonceType extends AbstractType
 {
@@ -46,6 +48,11 @@ class AnnonceType extends AbstractType
             ->add('content', TextareaType::class, $this->getConfiguration('Description détaillée','Donnez une description de votre bien'))
             ->add('rooms', IntegerType::class, $this->getConfiguration('Nombre de chambre','Donnez le nombre de chambres disponibles'))
             ->add('price', MoneyType::class, $this->getConfiguration('Prix par nuit','Indiquer le prix que vous voulez pour une nuit'))
+            ->add('images', CollectionType::class, [
+                'entry_type' => ImageType::class,
+                'allow_add' => true, // pour le data_prototype
+                'allow_delete' => true
+            ])
         ;
     }
 
@@ -56,3 +63,5 @@ class AnnonceType extends AbstractType
         ]);
     }
 }
+
+
