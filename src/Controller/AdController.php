e3579d6b48c28d99b9df6374df3e5d1073929dff
berti <berti@CLASSES.LOCAL>
2023-10-13 13:54:59 +0200

    cours 4 - partials base.html.twig

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -55,7 +55,10 @@ class AdController extends AbstractController
             // j'envoie les persistances dans la bdd
             $manager->flush();
 
-            $this->addFlash('success', "L'annonce <strong>".$ad->getTitle()."</strong> a bien été enregistrée");
+            $this->addFlash(
+                'success', 
+                "L'annonce <strong>".$ad->getTitle()."</strong> a bien été enregistrée"
+            );
 
             return $this->redirectToRoute('ads_show',[
                 'slug' => $ad->getSlug()
