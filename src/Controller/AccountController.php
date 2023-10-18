686bc80a194e0536bfa52231fde3220f082d4655
Boderio <boderiotrigan@hotmail.com>
2023-10-18 09:28:58 +0200

    cours 6 - user connexion v2

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
 {
-    
+    /**
+     * Permet à l'utilisateur de se connecter
+     *
+     * @param AuthenticationUtils $utils
+     * @return Response
+     */   
     #[Route('/login', name: 'account_login')]
-    public function index(): Response
+    public function index(AuthenticationUtils $utils): Response
     {
+        $error = $utils->getLastAuthenticationError();
+        $username = $utils->getLastUsername();
+
+
         return $this->render('account/index.html.twig', [
-            'controller_name' => 'AccountController',
+            'hasError' => $error !== null,
+            'username' => $username
         ]);
     }
 
