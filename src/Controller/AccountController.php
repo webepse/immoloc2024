d759412aa8bde547487fdeebde74ae6b111dd42d
Boderio <boderiotrigan@hotmail.com>
2023-10-18 09:59:01 +0200

    cours 6 - throttling

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Core\Exception\TooManyLoginAttemptsAuthenticationException;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AccountController extends AbstractController
@@ -21,10 +22,20 @@ class AccountController extends AbstractController
         $error = $utils->getLastAuthenticationError();
         $username = $utils->getLastUsername();
 
+        $loginError = null;
 
+        //dump($error);
+
+        if($error instanceof TooManyLoginAttemptsAuthenticationException)
+        {
+            // l'ereur est due à la limitation de tentative de connexion
+            $loginError = "Trop de tentatives de connexion. Réessayez plus tard";
+        }
+        
         return $this->render('account/index.html.twig', [
             'hasError' => $error !== null,
-            'username' => $username
+            'username' => $username,
+            'loginError' => $loginError
         ]);
     }
 
