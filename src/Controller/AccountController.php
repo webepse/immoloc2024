eae27d4eefc7184a769398edacb08f9f06042ca2
berti <berti@CLASSES.LOCAL>
2023-10-20 15:51:15 +0200

    cours 8 - edition image

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,6 +4,8 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\AccountType;
+use App\Form\ImgModifyType;
+use App\Entity\UserImgModify;
 use App\Entity\PasswordUpdate;
 use App\Form\RegistrationType;
 use App\Form\PasswordUpdateType;
@@ -207,4 +209,88 @@ class AccountController extends AbstractController
 
     }
 
+    /**
+     * Permet de supprimer l'image de l'utilisateur
+     *
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route("/account/delimg", name:"account_delimg")]
+    public function removeImg(EntityManagerInterface $manager): Response
+    {
+        $user = $this->getUser();
+        if(!empty($user->getPicture()))
+        {
+            unlink($this->getParameter('uploads_directory').'/'.$user->getPicture());
+            $user->setPicture('');
+            $manager->persist($user);
+            $manager->flush();
+            $this->addFlash(
+                'success',
+                'Votre avatar a bien été supprimé'
+            );
+        }
+
+        return $this->redirectToRoute('homepage');
+
+    }
+
+    /**
+     * Permet de modifier l'avatar de l'utilisateur
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route("/account/imgmodify", name:"account_modifimg")]
+    public function imgModify(Request $request, EntityManagerInterface $manager): Response
+    {
+        $imgModify = new UserImgModify();
+        $user = $this->getUser();
+        $form = $this->createForm(ImgModifyType::class, $imgModify);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            //permet de supprimer l'image dans le dossier
+            // gestion de la non-obligation de l'image
+            if(!empty($user->getPicture()))
+            {
+                unlink($this->getParameter('uploads_directory').'/'.$user->getPicture());
+            }
+
+              // gestion de l'image
+              $file = $form['newPicture']->getData();
+              if(!empty($file))
+              {
+                  $originalFilename = pathinfo($file->getClientOriginalName(), PATHINFO_FILENAME);
+                  $safeFilename = transliterator_transliterate('Any-Latin; Latin-ASCII; [^A-Za-z0-9_] remove; Lower()', $originalFilename);
+                  $newFilename = $safeFilename."-".uniqid().'.'.$file->guessExtension();
+                  try{
+                      $file->move(
+                          $this->getParameter('uploads_directory'),
+                          $newFilename
+                      );
+                  }catch(FileException $e)
+                  {
+                      return $e->getMessage();
+                  }
+                  $user->setPicture($newFilename);
+              }
+              $manager->persist($user);
+              $manager->flush();
+
+              $this->addFlash(
+                'success',
+                'Votre avatar a bien été modifié'
+              );
+
+              return $this->redirectToRoute('homepage');
+
+        }
+
+        return $this->render("account/imgModify.html.twig",[
+            'myForm' => $form->createView()
+        ]);
+    }
 }
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -34,7 +34,7 @@ class AppFixtures extends Fixture
         {
             $user = new User();
             $genre = $faker->randomElement($genres);
-            $picture = 'https://picsum.photos/seed/picsum/500/500';
+       
 
             $hash = $this->passwordHasher->hashPassword($user, 'password');
 
@@ -44,7 +44,7 @@ class AppFixtures extends Fixture
                 ->setIntroduction($faker->sentence())
                 ->setDescription('<p>'.join('</p><p>',$faker->paragraphs(3)).'</p>')
                 ->setPassword($hash)
-                ->setPicture($picture);
+                ->setPicture('');
 
             $manager->persist($user);    
 
