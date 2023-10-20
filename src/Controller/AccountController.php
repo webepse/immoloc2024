fe8e0f89d88fb82d431a8a5aeaf99f8e7b7bc632
berti <berti@CLASSES.LOCAL>
2023-10-20 13:25:52 +0200

    cours 7 - edition sans password

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\AccountType;
 use App\Form\RegistrationType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Request;
@@ -88,6 +89,37 @@ class AccountController extends AbstractController
         return $this->render("account/registration.html.twig",[
             'myForm' => $form->createView()
         ]);
+    }
+
+    /**
+     * Permet de modifier l'utilisateur
+     *
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return Response
+     */
+    #[Route("/account/profile", name:"account_profile")]
+    public function profile(Request $request, EntityManagerInterface $manager): Response
+    {
+        $user = $this->getUser(); // permet de récup l'utilisateur connecté
+        $form = $this->createForm(AccountType::class,$user);
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Les données ont été enregistrées avec succés"
+            );
+        }
+
+        return $this->render("account/profile.html.twig",[
+            'myForm' => $form->createView()
+        ]);
+
     }
 
 }
